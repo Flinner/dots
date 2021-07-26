6818a85b64761f2f3a490f2f9ea83ca2d600bc7e
Flinner <flinner.hlqn@aleeas.com>
2021-07-26 14:37:02 +0300

    feat: qutebrowser add adblock lists

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -683,6 +683,8 @@ config.bind("cec", "open javascript:location.href='org-protocol://roam-ref?templ
 ## URL-decoding it).
 ## Type: List of Url
 # c.content.blocking.adblock.lists = ['https://easylist.to/easylist/easylist.txt', 'https://easylist.to/easylist/easyprivacy.txt']
+c.content.blocking.adblock.lists = ['https://easylist.to/easylist/easylist.txt', 'https://easylist.to/easylist/easyprivacy.txt', 'https://easylist-downloads.adblockplus.org/easylistdutch.txt', 'https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt', 'https://www.i-dont-care-about-cookies.eu/abp/', 'https://secure.fanboy.co.nz/fanboy-cookiemonster.txt']
+
 
 ## Enable the ad/host blocker
 ## Type: Bool
