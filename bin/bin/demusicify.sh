51eb11db1604d70b25bf69dae32f43271a989f70
Flinner Yuu <flinner@nand.sh>
2023-03-31 13:28:16 +0300

    feat: idk

--- bin/bin/demusicify.sh
+++ bin/bin/demusicify.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
 
+PYTORCH_NO_CUDA_MEMORY_CACHING=1
+
 [ -z "$2" ] && echo "usage: input.mp4 output.mp4" 
 [ -z "$2" ] && exit
 
 VIDEO="$1"
 VIDEO_OUTPUT="$2"
 
-TMP_RAW_AUDIO=$(mktemp --suffix ".mp3")
-TMP_VOCALS_DIR=$(mktemp --directory)
-TMP_VOCALS=$(mktemp --suffix ".mp3")
+mkdir -p ~/tmp/demusicify
+echo "Created ~/tmp/demusicify"
+
+echo "Using ~/tmp/demusicify as a temp directory"
+TMP_RAW_AUDIO=$(mktemp -p ~/tmp/demusicify --suffix ".mp3")
+TMP_VOCALS_DIR=$(mktemp -p ~/tmp/demusicify --directory)
+TMP_VOCALS=$(mktemp -p ~/tmp/demusicify --suffix ".mp3")
+
+function cleanup () {
+
+    read -p "Do you want to cleanup? y/n" -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+    then
+        [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+        echo "Cleaning up!"
+        rm "$TMP_RAW_AUDIO"
+        rm "$TMP_VOCALS"
+        rm -rf "$TMP_VOCALS_DIR"
+    fi
+}
+trap cleanup EXIT
 
 # Extract Audio from Video
 ffmpeg -i "$VIDEO" -vn "$TMP_RAW_AUDIO" -y
@@ -16,7 +37,7 @@ ffmpeg -i "$VIDEO" -vn "$TMP_RAW_AUDIO" -y
 # move to a tmp file, becuase demucs creates a lot of garbage
 pushd "$TMP_VOCALS_DIR" || exit
 
-demucs --two-stems=vocals --segment 10 "$TMP_RAW_AUDIO" 
+demucs --two-stems=vocals --segment 15 "$TMP_RAW_AUDIO" 
 
 cp ./separated/*/*/vocals.wav "$TMP_VOCALS"
 
@@ -29,8 +50,3 @@ ffmpeg \
     -c:v copy \
     -map 0 -map 1:a \
     -y "$VIDEO_OUTPUT"
-
-
-rm "$TMP_RAW_AUDIO"
-rm "$TMP_VOCALS"
-rm -rf "$TMP_VOCALS_DIR"
