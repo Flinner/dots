5fc148b2089f5d1fda1b243e3d158f31ba8d6e94
Flinner Yuu <flinner@nand.sh>
2026-03-05 02:20:49 +0300

    feat: better julia startup file

--- julia/.julia/config/startup.jl
+++ julia/.julia/config/startup.jl
@@ -1,5 +1,24 @@
+using Pkg
+if isfile("Project.toml") && isfile("Manifest.toml")
+    Pkg.activate(".")
+end
+
+
 try
     using Revise
 catch e
     @warn "Error initializing Revise" exception=(e, catch_backtrace())
+end
+
+try
+    using BenchmarkTools
+catch e
+    @warn "Error initializing Revise" exception=(e, catch_backtrace())
+end
+
+
+try
+     @eval using OhMyREPL
+catch e
+     @warn "error while importing OhMyREPL" e
 end
